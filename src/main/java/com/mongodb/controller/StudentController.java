f1daa89a00c9cf74c247497212338f7ae47d70b7
vijay-jaybhay <vijay.jaybhay@lumiq.ai>
2024-01-10 23:25:38 +0530

    cr-1234 code pushed

--- src/main/java/com/mongodb/controller/StudentController.java
+++ src/main/java/com/mongodb/controller/StudentController.java
@@ -2,15 +2,18 @@ package com.mongodb.controller;
 
 import com.mongodb.model.Student;
 import com.mongodb.service.StudentService;
+import lombok.extern.java.Log;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.logging.Level;
+
 @RestController
-@Slf4j
 @RequestMapping("/student")
+@Log
 public class StudentController {
 
 	@Autowired
@@ -19,15 +22,23 @@ public class StudentController {
 	@GetMapping("/hello")
 	public ResponseEntity<String> sayHello(){
 		String message =" Hi All Students From Pune Univercity";
-		log.info("Inside say Hello method {}",message);
 		return ResponseEntity.ok(message);
 	}
-// Comment added for checking
+	// Comment added for checking
 	@PostMapping("/save")
 	public ResponseEntity<String> saveStudent(@RequestBody Student student){
 		Student std = studentService.createStudent(student);
-		log.info("inside save method  {}", std);
 		return ResponseEntity.status(HttpStatus.CREATED).body("Saved Successfully");
 
 	}
+	@GetMapping("/getMessage")
+	public ResponseEntity<String> getMessage(@RequestBody Student student){
+		Student std = studentService.createStudent(student);
+
+		log.log(Level.INFO," This code is of UAT and CR-1234");
+
+		return ResponseEntity.status(HttpStatus.CREATED).body("Saved Successfully");
+
+	}
+
 }

fbadeefbf6b860076ef355b67ccc86e16a222f18
vijay-jaybhay <vijay.jaybhay@lumiq.ai>
2024-01-10 23:03:59 +0530

    branch name log added

--- src/main/java/com/mongodb/controller/StudentController.java
+++ src/main/java/com/mongodb/controller/StudentController.java
@@ -1,16 +1,21 @@
 package com.mongodb.controller;
 
+import ch.qos.logback.core.joran.spi.ConsoleTarget;
 import com.mongodb.model.Student;
 import com.mongodb.service.StudentService;
+import lombok.extern.java.Log;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.logging.Level;
+
 @RestController
 @Slf4j
 @RequestMapping("/student")
+@Log
 public class StudentController {
 
 	@Autowired
@@ -19,15 +24,26 @@ public class StudentController {
 	@GetMapping("/hello")
 	public ResponseEntity<String> sayHello(){
 		String message =" Hi All Students From Pune Univercity";
-		log.info("Inside say Hello method {}",message);
+//		log.info("Inside say Hello method {}",message);
+		log.log(Level.INFO," Message is "+message);
 		return ResponseEntity.ok(message);
 	}
 // Comment added for checking
 	@PostMapping("/save")
 	public ResponseEntity<String> saveStudent(@RequestBody Student student){
 		Student std = studentService.createStudent(student);
-		log.info("inside save method  {}", std);
+//		log.info("inside save method  {}", std);
 		return ResponseEntity.status(HttpStatus.CREATED).body("Saved Successfully");
 
 	}
+	@GetMapping("/getMessage")
+	public ResponseEntity<String> getMessage(@RequestBody Student student){
+		Student std = studentService.createStudent(student);
+
+		log.log(Level.INFO," This logger is of CODEBUILD-DEV branch");
+
+		return ResponseEntity.status(HttpStatus.CREATED).body("Saved Successfully");
+
+	}
+
 }
